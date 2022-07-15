a6d36df63375dbe23740e993f1b01bf306912f92
Cole20021 <cam.leal@duocuc.cl>
2022-07-15 05:03:40 -0400

    ET_Mascota_Jardineria_CamiloLealEncalada_003D

---
+++ Mascotas/core/context_processor.py
@@ -0,0 +1,11 @@
+
+
+def total_carrito(request):
+    total = 0
+    if request.user.is_authenticated:
+
+        if 'carrito' in request.session:
+            for key,value in request.session['carrito'].items():
+                total = total +(float(value['precio'])*value['acumulado'])
+
+    return {'total_carrito':total}
